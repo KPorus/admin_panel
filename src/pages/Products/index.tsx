3f35196e2153f13c98dab705d3f03600175283cf
KPorus <designerfardin2021@gmail.com>
2024-04-05 21:55:33 +0600

    modifid

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,17 +1,49 @@
+import { Input, Tooltip } from 'antd';
 import dynamic from 'next/dynamic';
 import React, { ReactNode } from 'react';
-const SideBar = dynamic(() => import("@/components/Layout/Sidebar"));
-const index = () => {
+import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
+import { useForm, SubmitHandler } from 'react-hook-form';  // Ensure correct import
+
+const SideBar = dynamic(() => import('@/components/Layout/Sidebar'));
+
+type InputsData = {
+    productName: string;
+    catagory: string;
+};
+
+const Index = () =>
+{
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<InputsData>();  // Correct type parameter
+
+    const onSubmit: SubmitHandler<InputsData> = (data) => console.log(data);
+
     return (
-        <div>
-           All products 
+        <div className='text-start p-3 uppercase text-slate-900 font-medium'>
+            All products <hr className='w-44 relative bottom-[0.6rem] right-[-122px]'></hr>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    {...register('productName')}
+                    placeholder='Enter Product name'
+                    prefix={<UserOutlined className='site-form-item-icon' />}
+                    suffix={
+                        <Tooltip title='Product name'>
+                            <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+                        </Tooltip>
+                    }
+                />
+                <input type='submit' />
+            </form>
         </div>
     );
 };
 
-export default index;
+export default Index;  // Correct component name
 
-index.getLayout = function getLayout(page: ReactNode)
+Index.getLayout = function getLayout(page: ReactNode)
 {
     return <SideBar>{page}</SideBar>;
 };
